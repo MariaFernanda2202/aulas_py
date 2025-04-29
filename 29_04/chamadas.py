2aaac66efbc9e8b3806cda337209a0643a36cd04
Maria Fernanda2202 <bubu.ramos.f@gmail.com>
2025-04-29 20:12:08 -0300

    Comitando certo

---
+++ 29_04/chamadas.py
@@ -0,0 +1,3 @@
+from funcoes import par_ou_impar
+#funcoes.=> para chamar as funçoes 
+par_ou_impar(2)
---
+++ 29_04/funcoes.py
@@ -0,0 +1,22 @@
+# def - definição -> abre função
+# exemplo de função
+
+def soma_dois_numeros(numero1, numero2):
+    resultado= numero1+numero2
+    return resultado #retorna o valor para fora da função
+
+#print(f"Resultado da função: {soma_dois_numeros(10, 20)}")
+
+
+def par_ou_impar(numero : int): #essa função se chama procedural
+    if numero%2==0:
+        print("Numero é par")
+    else:
+        print("Numero é impar")
+
+#par_ou_impar(int(input("Digite um número inteiro: ")))
+# par_ou_impar(5) = se quiser mostrar um numero sem fazer um input
+
+
+
+

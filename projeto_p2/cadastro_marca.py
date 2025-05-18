9cf7f27bd5db17c736c6ba318e1faccd385c9cd5
Maria Fernanda2202 <bubu.ramos.f@gmail.com>
2025-05-18 14:19:26 -0300

    fazendo o cadastro certo

--- projeto_p2/cadastro_marca.py
+++ projeto_p2/cadastro_marca.py
@@ -3,8 +3,7 @@ import os
 if not os.path.exists("banco_de_dados"): #verifica se o diretorio não existe  
     os.makedirs("banco_de_dados")
 
-marca=[]
-produto=[]
+marcas=[]
 while True:
     opcao=int(input("Adicionar uma marca 0 - sair 1: "))
     
@@ -14,11 +13,13 @@ while True:
     
     qtd_marcas=int(input("Quantas marcas deseja adicionar? "))
     for _ in range(0, qtd_marcas):
-        marca.append(input("Qual a marca? "))
+        marca=input("Qual a marca? ")
 
         qtd_produtos=int(input("Quantos produtos dessa marca deseja adicionar? "))
+        produtos=[]
         for i in range(0, qtd_produtos):   
-            produto.append(input("Qual o produto"[i]))
+            produtos.append(input("Qual o produto"))
+        marcas.append({"marca":marca, "produtos":produtos})
     
 
     
@@ -27,8 +28,15 @@ while True:
 
 
 with open("banco_de_dados/marcas.txt", "a+") as arquivo:
-    for indice, marcas in enumerate(produto): #percorre a lista de usuarios 
-        arquivo.write(marca + ":" + produto[indice]+"\n") #escreve o usuario
+    for indice_marca, marca in enumerate(marcas): #percorre a lista de usuarios 
+        print(marca["marca"])
+        for indice_produto, produto in enumerate(marcas[indice_marca]["produtos"]):
+            print(produto)
+
+            arquivo.write(marca["marca"] + ":" +produto+"\n") #escreve o usuario
+
+
+
 
     # for marca in range(0, qtd_marca):
     # marcas.append(input("Qual marca gostaria de adicionar: "))
